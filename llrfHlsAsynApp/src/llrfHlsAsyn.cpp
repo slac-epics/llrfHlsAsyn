7d64aa762bc9fd2a0ba7894ed2167aaa6e857b57
Kukhee Kim <khkim@slac.stanford.edu>
2020-05-18 14:54:40 -0700

    add asyn parameters and parameter setup function

--- llrfHlsAsynApp/src/llrfHlsAsyn.cpp
+++ llrfHlsAsynApp/src/llrfHlsAsyn.cpp
@@ -41,7 +41,7 @@ llrfHlsAsynDriver::llrfHlsAsynDriver(const char *portName, const char *pathStrin
     : asynPortDriver(portName,
                      1, /* number of elements of this device */
 #if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
-                     NUM_ATCACOMMON_DET_PARAMS, /* number of asyn params of be cleared for each device */
+                     NUM_LLRFHLS_DET_PARAMS, /* number of asyn params of be cleared for each device */
 #endif  /* asyn version check, under 4.32 */
                      asynInt32Mask | asynFloat64Mask | asynOctetMask | asynDrvUserMask | asynInt16ArrayMask | asynInt32ArrayMask | asynFloat64ArrayMask, /* Interface mask */
                      asynInt32Mask | asynFloat64Mask | asynOctetMask | asynEnumMask    | asynInt16ArrayMask | asynInt32ArrayMask | asynFloat64ArrayMask,  /* Interrupt mask */
@@ -53,3 +53,53 @@ llrfHlsAsynDriver::llrfHlsAsynDriver(const char *portName, const char *pathStrin
 }
 
 llrfHlsAsynDriver::~llrfHlsAsynDriver() {}
+
+
+
+void llrfHlsAsynDriver::ParameterSetup(void)
+{
+    char param_name[80];
+
+    sprintf(param_name, P_REF_OFFSET_STR);           createParam(param_name, asynParamFloat64, &p_p_ref_offset);
+    sprintf(param_name, P_FB_OFFSET_STR);            createParam(param_name, asynParamFloat64, &p_p_fb_offset);
+    sprintf(param_name, P_GAIN_STR);                 createParam(param_name, asynParamFloat64, &p_p_gain);
+    sprintf(param_name, A_GAIN_STR);                 createParam(param_name, asynParamFloat64, &p_a_gain);
+    sprintf(param_name, REF_SUBTRACTION_ENABLE_STR); createParam(param_name, asynParamInt32,   &p_ref_subtraction_enable);
+    sprintf(param_name, FB_PHASE_ENABLE_STR);        createParam(param_name, asynParamInt32,   &p_fb_phase_enable);
+    sprintf(param_name, FB_AMPL_ENABLE_STR);         createParam(param_name, asynParamInt32,   &p_fb_ampl_enable);
+    sprintf(param_name, P_CORR_UPPER_STR);           createParam(param_name, asynParamFloat64, &p_p_corr_upper);
+    sprintf(param_name, P_CORR_LOWER_STR);           createParam(param_name, asynParamFloat64, &p_p_corr_lower);
+    sprintf(param_name, A_CORR_UPPER_STR);           createParam(param_name, asynParamFloat64, &p_a_corr_upper);
+    sprintf(param_name, A_CORR_LOWER_STR);           createParam(param_name, asynParamFloat64, &p_a_corr_lower);
+    sprintf(param_name, A_DRV_UPPER_STR);            createParam(param_name, asynParamFloat64, &p_a_drv_upper);
+    sprintf(param_name, A_DRV_LOWER_STR);            createParam(param_name, asynParamFloat64, &p_a_drv_lower);
+
+    sprintf(param_name, AVG_WINDOW_STR);             createParam(param_name, asynParamFloat64Array, &p_avg_window);
+
+    for(int i = 0; i < NUM_CH; i++) {    // for loop for number of channels
+        sprintf(param_name, REF_WEIGHT_STR, i); createParam(param_name, asynParamFloat64, &(p_ref_weight_ch[i]));
+        sprintf(param_name, FB_WEIGHT_STR,  i); createParam(param_name, asynParamFloat64, &(p_fb_weight_ch[i]));
+        sprintf(param_name, P_OFFSET_STR,   i); createParam(param_name, asynParamFloat64, &(p_p_offset_ch[i]));
+
+        sprintf(param_name, P_CH_STR,       i); createParam(param_name, asynParamFloat64, &(p_p_ch[i]));
+        sprintf(param_name, A_CH_STR,       i); createParam(param_name, asynParamFloat64, &(p_a_ch[i]));
+
+        sprintf(param_name, I_WF_STR,       i); createParam(param_name, asynParamFloat64Array, &(p_i_wf_ch[i]));
+        sprintf(param_name, Q_WF_STR,       i); createParam(param_name, asynParamFloat64Array, &(p_q_wf_ch[i]));
+       
+    }
+
+    for(int i = 0; i < NUM_TIMESLOT; i++) {    // for loop for number of timeslots
+        sprintf(param_name, P_DES_STR, i); createParam(param_name, asynParamFloat64, &(p_p_des_ts[i]));
+        sprintf(param_name, A_DES_STR, i); createParam(param_name, asynParamFloat64, &(p_a_des_ts[i]));
+
+        sprintf(param_name, P_FB_STR,  i); createParam(param_name, asynParamFloat64, &(p_p_fb_ts[i]));
+        sprintf(param_name, A_FB_STR,  i); createParam(param_name, asynParamFloat64, &(p_a_fb_ts[i]));
+        sprintf(param_name, P_REF_STR, i); createParam(param_name, asynParamFloat64, &(p_p_ref_ts[i]));
+        sprintf(param_name, A_REF_STR, i); createParam(param_name, asynParamFloat64, &(p_a_ref_ts[i]));
+        sprintf(param_name, P_SET_STR, i); createParam(param_name, asynParamFloat64, &(p_p_set_ts[i]));
+        sprintf(param_name, A_SET_STR, i); createParam(param_name, asynParamFloat64, &(p_a_set_ts[i]));
+
+
+    }
+}
--- llrfHlsAsynApp/src/llrfHlsAsyn.h
+++ llrfHlsAsynApp/src/llrfHlsAsyn.h
@@ -23,6 +23,7 @@
 #define MAX_SAMPLES      4096      // number of samples in a waveform
 
 
+
 class llrfHlsAsynDriver
     :asynPortDriver {
     public:
@@ -33,9 +34,56 @@ class llrfHlsAsynDriver
         char *port;
         char *path;
 
+        void ParameterSetup(void);
+
 
     protected:
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
+        int firstLlrfHlsParam;
+#define FIRST_LLRFHLS_PARAM   firstLlrfHlsParam
+#endif /* ASYN VERSION CHECK under 4.32 */
+
+        int p_p_ref_offset;                   // phase offset of reference
+        int p_p_fb_offset;                    // phase offset for feedback
+        int p_p_gain;                         // gain for phase loop
+        int p_a_gain;                         // gain for amplitude loop
+        int p_ref_subtraction_enable;         // reference subtraction control enable/disable
+        int p_fb_phase_enable;                // phase loop control, enable/disable
+        int p_fb_ampl_enable;                 // amplitude loop control, enable/disable
+        int p_p_corr_upper;                   // phase correction upper limit
+        int p_p_corr_lower;                   // phase correction lower limit
+        int p_a_corr_upper;                   // amplitude correction upper limit
+        int p_a_corr_lower;                   // amplitude correction lower limit
+        int p_a_drv_upper;                    // amplitude drive upper limit
+        int p_a_drv_lower;                    // amplitude drive lower limit
+        int p_ref_weight_ch[NUM_CH];          // channel weight for reference
+        int p_fb_weight_ch[NUM_CH];           // channel weight for feedback
+        int p_p_offset_ch[NUM_CH];            // phase offset for each channel
+        int p_p_des_ts[NUM_TIMESLOT];         // dessired phase for each timeslot
+        int p_a_des_ts[NUM_TIMESLOT];         // desired amplitude for each timeslot
+        int p_p_ch[NUM_CH];                   // actual phase for each channel
+        int p_a_ch[NUM_CH];                   // actual amplitude for each channel
+        int p_p_fb_ts[NUM_TIMESLOT];          // phase for feedback for each timeslot
+        int p_a_fb_ts[NUM_TIMESLOT];          // amplitude for feedback for each timeslot
+        int p_p_ref_ts[NUM_TIMESLOT];         // reference phase for each timeslot
+        int p_a_ref_ts[NUM_TIMESLOT];         // reference amplitude for each timeslot
+        int p_p_set_ts[NUM_TIMESLOT];         // phase ser value for each timeslot
+        int p_a_set_ts[NUM_TIMESLOT];         // amplitude set value for each timeslot
+        int p_avg_window;                     // average window
+        int p_i_wf_ch[NUM_CH];                // i waveform for each channel
+        int p_q_wf_ch[NUM_CH];                // q waveform for each channel
+
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)      
+        int lastLlrfHlsParam;
+#define LAST_LLRFHLS_PARAM   lastLlrfHlsParam
+#endif /* asyn version check, under 4.32 */
+
 };
+
+
+#if (ASYN_VERSION <<8 | ASYN_REVISION) < (4<<8 | 32)
+#define NUM_LLRFHLS_DET_PARAMS ((int)(&LAST_LLRFHLS_PARAM - &FIRST_LLRFHLS_PARAM-1))
+#endif /* asyn version check, under 4.32 */
 
 
 #define P_REF_OFFSET_STR             "p_ref_offset"      // phase offset for reference, in degree
@@ -51,7 +99,7 @@ class llrfHlsAsynDriver
 #define A_CORR_LOWER_STR             "a_corr_lower"      // amplitude correction lower limit
 #define A_DRV_UPPER_STR              "a_drv_upper"       // amplitude drive upper limit
 #define A_DRV_LOWER_STR              "a_drv_lower"       // amplitude drive lower limit
-#define REF_WIGHT_STR                "ref_weight_ch%d"   // weight average for reference, for each channel, array[10]
+#define REF_WEIGHT_STR               "ref_weight_ch%d"   // weight average for reference, for each channel, array[10]
 #define FB_WEIGHT_STR                "fb_weight_ch%d"    // weight average for feedback, for each channel,  array[10]
 #define P_OFFSET_STR                 "p_offset_ch%d"     // phase offset, for each channel, array[10]
 #define P_DES_STR                    "p_des_ts%d"        // desired phase, for each timeslot, array[18]
@@ -60,7 +108,7 @@ class llrfHlsAsynDriver
 #define A_CH_STR                     "a_act_ch%d"        // actual amplitude, for each channel, array[10]
 #define P_FB_STR                     "p_fb_ts%d"         // actual phase for feedback, for each timeslot, array[18]
 #define A_FB_STR                     "a_fb_ts%d"         // actual amplitude for feedback, for each timeslot, array[18]
-#define P_REF_STR                    "p_ref_ts%d",       // actual phase for reference, for each timeslot,  array[18]
+#define P_REF_STR                    "p_ref_ts%d"        // actual phase for reference, for each timeslot,  array[18]
 #define A_REF_STR                    "a_ref_ts%d"        // actual amplitude for reference, for each timeslot, array[18]
 #define P_SET_STR                    "p_set_ts%d"        // phase set value, for each timeslot, array[18]
 #define A_SET_STR                    "a_set_ts%d"        // amplitude set value, for each timeslot, array[18]
